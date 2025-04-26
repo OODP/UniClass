43f61d31ce1b241007eec4ff78f4ab3fef1a74b2
Sumin Jeon <wjstnals1211@gmail.com>
2025-04-26 22:14:56 +0900

    initial project setup

---
+++ src/User.java
@@ -0,0 +1,45 @@
+abstract public class User {
+
+    private final String id;
+    private final String password;
+    private final String name;
+    private final String uniqueId;
+
+    public User(String id, String password, String name, String uniqueId) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.uniqueId = uniqueId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUniqueId() {
+        return uniqueId;
+    }
+
+    // login
+    public void login() {
+        // Implement login logic here
+    }
+
+    // logout
+    public void logout() {
+        // Implement logout logic here
+    }
+
+    // signUp
+    public void signUp() {
+        // Implement registration logic here
+    }
+}
