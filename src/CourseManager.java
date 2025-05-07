f6b8694558c4ebc3e83867ee3ddb04d46eeadb71
JunYong An <happihanjy@naver.com>
2025-05-07 21:50:41 +0900

    Iteration2 기능 구현 완료

--- src/CourseManager.java
+++ src/CourseManager.java
@@ -1,10 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class CourseManager {
 
     private static final List<Course> pendingCourses = new ArrayList<>(); // 개설 검토중인 과목 리스트
     private static final List<Course> openedCourses = new ArrayList<>(); // 개설된 과목 리스트
+
+    // 초기 과목 개설 리스트 설정
+    static {
+        Professor professor = new Professor("prof", "1234", "교수 A", "P001");
+        Course course1 = new Course("CS101", "운영체재", 3, 30, professor);
+        Course course2 = new Course("CS102", "알고리즘", 3, 30, professor);
+        openedCourses.add(course1);
+        openedCourses.add(course2);
+    }
 
     // 과목 개설 요청
     public void requestOpenCourse(String courseId, String courseName, int credit, int participants, Professor professor) {
@@ -55,8 +65,58 @@ public class CourseManager {
         return pendingCourses;
     }
 
+
+    // -------------------Iteration 2 -------------------
+    // 수강 신청
+    Scanner sc = new Scanner(System.in);
+    public void applyCourse(Student student) {
+        List<Course> openedCourses = getOpenedCourses();
+        for(Course course : openedCourses) {
+            System.out.println(" - " + course.getCourseId() + ": " + course.getCourseName() + " (" +
+                    course.getCredit() + "학점, " + course.getParticipants() + "명 수강 가능)");
+        }
+
+        System.out.print(" - 수강 신청할 과목 코드 입력: ");
+        String courseId = sc.next();
+
+        for (Course course : openedCourses) {
+            if (course.getCourseId().equals(courseId)) {
+                student.getCourseList().add(course);
+                break;
+            }
+        }
+        System.out.println("✅ 수강 신청 완료!");
+    }
+
+    //수강 취소
+    public  void cancelCourse(Student student){
+        List<Course> courseList = student.getCourseList();
+        if (courseList.isEmpty()) {
+            System.out.println("⚠️ 수강 신청한 과목이 없습니다.");
+            return;
+        }
+
+        for (Course course : courseList) {
+            System.out.println(" - " + course.getCourseId() + ": " + course.getCourseName() +
+                    " (" + course.getCredit() + "학점, " + course.getParticipants() + "명 수강 가능)");
+        }
+
+        System.out.print(" - 수강 취소할 과목 코드 입력: ");
+        String courseId = sc.next();
+
+        for (Course course : courseList) {
+            if (course.getCourseId().equals(courseId)) {
+                student.getCourseList().remove(course);
+                break;
+            }
+        }
+        System.out.println("✅ 수강 취소 완료!");
+    }
+    //수강 과목 조회
+
     // 개설된 과목 조회
     public List<Course> getOpenedCourses() {
         return openedCourses;
     }
+
 }
